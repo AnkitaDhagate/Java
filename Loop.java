9634d1ed525f1904b141e44d891586b56e1f13a3
Ankita Dhagate <159905476+AnkitaDhagate@users.noreply.github.com>
2024-08-02 20:47:26 +0530

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Ankita
+{
+    public static void main(String[]args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i=3;
+
+        while(i>n)
+        {
+            System.out.println(i);
+
+            if(i==5)
+            {
+                break;
+            }
+            System.out.println("Good");
+        }
+        System.out.println("Ganpati Bappa Morya");
+
+    }
+}
